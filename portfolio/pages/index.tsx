8c87f01ccd9f44105789156d3c2f3afd170007c6
Ravi_30 <rvadher@horizontal.com>
2023-07-13 19:05:25 +0530

    changes in index file

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import Navbar from "@/Components/Navbar";
 
 import React from "react";
 import Hero from "@/Components/Hero/Hero";
